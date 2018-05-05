a534093b2c7481948b152d718dd807aab926349c
mp3jjk <jhjung@lanada.kaist.ac.kr>
2018-05-06 03:54:02 +0900

    G2 script

--- lanada/sim_scripts/run.sh
+++ lanada/sim_scripts/run.sh
@@ -3,26 +3,26 @@
 SR=0 # Decide whether SR simulation runs or not
 LR=1 # For LR case
 ONLY_LONG=0 # SR = 1 with only Long
-WAKE_UP=0 # LR = 1 with Wake-up radio
+WAKE_UP=1 # LR = 1 with Wake-up radio
 TRAFFIC=0 # 0 = periodic, 1 = poisson
-VAR_PERIOD=(5)
-VAR_ARRIVAL=(10 30 60)
+VAR_PERIOD=(25)
+VAR_ARRIVAL=(5 10)
 VAR_TOPOLOGY=("36grid_mrm2_cnt")
 VAR_LR_RANGE=("2X")
 VAR_LTMAX=("1")
 
-VAR_LR_WEIGHT=(2)
+VAR_LR_WEIGHT=(1)
 VAR_ETX_WEIGHT=1
-VAR_BETA=(2)
+VAR_BETA=(1)
 VAR_BETA_DIV=(1)
 VAR_CROSS_OPT=0
 
 VAR_STROBE_CNT=0
 VAR_CHECK_RATE=(8)
 
-DATE="G1"
-#SEED_NUMBER=("1" "2" "3" "4" "5")
-SEED_NUMBER=("1")
+DATE="G2"
+SEED_NUMBER=("1" "2" "3" "4" "5")
+#SEED_NUMBER=("1")
 MRM=1
 VAR_PERCENT=("1")
 
--- lanada_2/project-conf.h
+++ lanada_2/project-conf.h
@@ -58,7 +58,7 @@ uint8_t dead;
 
 #define TRAFFIC_MODEL 0 // 0: Periodic, 1: Poisson
 #if TRAFFIC_MODEL == 0
-#define PERIOD 15
+#define PERIOD 35
 #elif TRAFFIC_MODEL == 1
 #define ARRIVAL_RATE 0 // Mean value, 1/lambda
 #endif
