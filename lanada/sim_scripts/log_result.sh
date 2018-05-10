e25967d1053fee11f4eb07f30c2be210857e3ec8
mp3jjk <jhjung@lanada.kaist.ac.kr>
2018-05-10 17:06:57 +0900

    log_result with avr

--- lanada/sim_scripts/log_result.sh
+++ lanada/sim_scripts/log_result.sh
@@ -3,12 +3,14 @@
 TARGET=$1
 SERVER=$2
 
-rm lifetime_$TARGET_$SERVER
-rm prr_$TARGET_$SERVER
-rm delay_$TARGET_$SERVER
+rm *_$TARGET_$SERVER
 
 #echo "$TARGET" >> lifetime_$TARGET_$SERVER
 #echo "$TARGET" >> prr_$SERVER
 ./lifetime.sh $TARGET >> lifetime_$TARGET_$SERVER
 ./prr.sh $TARGET >> prr_$TARGET_$SERVER
 ./delay.sh $TARGET >> delay_$TARGET_$SERVER
+
+./avr.sh lifetime $TARGET >> avr_lifetime_$TARGET_$SERVER
+./avr.sh prr $TARGET >> avr_prr_$TARGET_$SERVER
+./avr.sh delay $TARGET >> avr_delay_$TARGET_$SERVER
