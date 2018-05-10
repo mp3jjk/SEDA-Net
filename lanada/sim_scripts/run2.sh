779e1613b94147183aaf7a5996389d9e7fdf0e77
mp3jjk <jhjung@lanada.kaist.ac.kr>
2018-05-10 16:03:29 +0900

    G9

--- lanada/sim_scripts/run2.sh
+++ lanada/sim_scripts/run2.sh
@@ -2,10 +2,10 @@
 
 SIM_TYPE=2 # 0: SR, 1: LR, 2: WAKE-UP, 3: One-Net
 
-TRAFFIC=1 # 0 = periodic, 1 = poisson
-VAR_PERIOD=(10 20 40)
+TRAFFIC=0 # 0 = periodic, 1 = poisson
+VAR_PERIOD=(20)
 VAR_ARRIVAL=(10 20 40)
-VAR_TOPOLOGY=("50random_mrm2_cnt")
+VAR_TOPOLOGY=("36grid_mrm2_cnt")
 VAR_LR_RANGE=("2X")
 VAR_LTMAX=("1")
 
@@ -16,13 +16,13 @@ VAR_BETA_DIV=(1)
 VAR_CROSS_OPT=0
 
 VAR_STROBE_CNT=0
-VAR_CHECK_RATE=(4)
+VAR_CHECK_RATE=(16)
 
-DATE="G8"
-SEED_NUMBER=("4" "5")
+DATE="G9"
+SEED_NUMBER=("3" "4")
 #SEED_NUMBER=("1")
 MRM=2
-VAR_PERCENT=("1")
+VAR_PERCENT=("8")
 
 if [ $SIM_TYPE -eq 0 ]
 then
